1bb13633cebff4cb019fce705fbeb9c4200c8352
TyomoGit <riku3dno.1@gmail.com>
2024-06-13 11:36:00 +0900

    init

---
+++ line_history.py
@@ -0,0 +1,5 @@
+from .line_history_py import *
+
+__doc__ = line_history_py.__doc__
+if hasattr(line_history_py, "__all__"):
+    __all__ = line_history_py.__all__
---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+use pyo3::exceptions::PyOSError;
+use pyo3::prelude::*;
+
+#[pyclass]
+pub struct LineContent {
+    pub date: chrono::NaiveDate,
+    pub line_count: usize,
+    pub line: String,
+}
+
+#[pymethods]
+impl LineContent {
+    #[new]
+    fn new(date: chrono::NaiveDate, line_count: usize, line: String) -> Self {
+        Self {
+            date,
+            line_count,
+            line,
+        }
+    }
+}
+
+impl From<::line_history::line_content::LineContent> for LineContent {
+    fn from(line_content: ::line_history::line_content::LineContent) -> Self {
+        let ::line_history::line_content::LineContent {
+            date,
+            line_count,
+            line,
+        } = line_content;
+        Self {
+            date,
+            line_count,
+            line,
+        }
+    }
+}
+
+#[pyclass]
+struct History {
+    history: line_history::history::History,
+}
+
+#[pymethods]
+impl History {
+    #[new]
+    fn new(data: &str) -> Self {
+        Self {
+            history: line_history::history::History::new(data),
+        }
+    }
+
+    #[staticmethod]
+    fn read_from_file(path: &str) -> PyResult<Self> {
+        let Ok(history) = line_history::history::History::read_from_file(path) else {
+            return Err(PyOSError::new_err("Failed to read from file"));
+        };
+
+        Ok(Self { history })
+    }
+
+    #[staticmethod]
+    fn from_lines(lines: Vec<String>) -> Self {
+        let result = ::line_history::history::History::from_lines(lines);
+        Self { history: result }
+    }
+
+    fn len(&self) -> usize {
+        self.history.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.history.is_empty()
+    }
+
+    fn search_by_date(&self, date: chrono::NaiveDate) -> PyResult<Option<String>> {
+        let result = self.history.search_by_date(&date);
+        Ok(result)
+    }
+
+    fn search_by_keyword(&self, keyword: &str) -> PyResult<Vec<LineContent>> {
+        let result = self
+            .history
+            .search_by_keyword(keyword)
+            .into_iter()
+            .map(|line_content| line_content.into())
+            .collect();
+
+        Ok(result)
+    }
+
+    fn search_by_random(&self) -> PyResult<String> {
+        let result = self.history.search_by_random();
+        Ok(result)
+    }
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn line_history_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<History>()?;
+    Ok(())
+}
